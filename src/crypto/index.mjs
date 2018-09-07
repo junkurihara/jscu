75b3372056e7a26ec2fd0135590c615fa4ba33c6
Jun Kurihara <junkurihara@users.noreply.github.com>
2018-09-07 18:05:41 +0900

    Fixed: removed unnecessary file

--- src/crypto/index.mjs
+++ src/crypto/index.mjs
@@ -12,7 +12,6 @@ const hkdf = util.hkdf;
 const aes = util.aes;
 const hmac = util.hmac;
 const jwkey = util.jwkey;
-const x509 = util.x509;
 
-export { sign, verify, generateKeyPair, encrypt, decrypt, keyconv, random, hash, hkdf, aes, hmac, jwkey, x509 };
+export { sign, verify, generateKeyPair, encrypt, decrypt, keyconv, random, hash, hkdf, aes, hmac, jwkey };
 
