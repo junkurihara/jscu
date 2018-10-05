94bf15f8eb0a7e0ca4cec424b113fffcfa8df739
Jun Kurihara <kurihara@ieee.org>
2018-10-05 20:00:34 +0900

    Updated: updated tests and package.json

--- packages/js_crypto_key_utils/test/convertEC.spec.js
+++ packages/js_crypto_key_utils/test/convertEC.spec.js
@@ -1,5 +1,5 @@
 import keyutils from '../src/index.js';
-import jscu from 'js-crypto-utils';
+import ec from 'js-crypto-ec/dist/index.js';
 
 import chai from 'chai';
 // const should = chai.should();
@@ -15,37 +15,35 @@ function objectSort(obj){
 
 const curves = ['P-256', 'P-384', 'P-521'];
 describe('EC Key conversion from/to JWK test.', () => {
-
-  const getKeyParam = (elem) => ({keyType: 'EC', namedCurve: elem});
+  
   let ECKeySet = [];
   before(async () => {
     ECKeySet = await Promise.all(curves.map(async (crv) => {
-      const param = getKeyParam(crv);
-      return await jscu.crypto.generateKeyPair(param);
+      return await ec.generateKey(crv);
     }));
   });
 
   it('JWK EC should be successfully converted to PEM and re-converted to JWK correctly', async () => {
     const array = ECKeySet.map( (key) => {
-      const pempub = keyutils.fromJwkTo('pem', key.publicKey.key, 'public', {compact: false});
-      const pempri = keyutils.fromJwkTo('pem', key.privateKey.key, 'private', {compact: false});
+      const pempub = keyutils.fromJwkTo('pem', key.publicKey, 'public', {compact: false});
+      const pempri = keyutils.fromJwkTo('pem', key.privateKey, 'private', {compact: false});
       // console.log(pempub);
       // console.log(pempri);
       const jwkpub = keyutils.toJwkFrom('pem', pempub, 'public');
       const jwkpri = keyutils.toJwkFrom('pem', pempri, 'private');
-      delete key.publicKey.key.ext;
-      delete key.privateKey.key.ext;
-      delete key.publicKey.key.alg;
-      delete key.privateKey.key.alg;
-      delete key.publicKey.key.key_ops;
-      delete key.privateKey.key.key_ops;
-      const res =  (JSON.stringify(objectSort(jwkpub)) === JSON.stringify(objectSort(key.publicKey.key)))
-          && (JSON.stringify(objectSort(jwkpri)) === JSON.stringify(objectSort(key.privateKey.key)));
+      delete key.publicKey.ext;
+      delete key.privateKey.ext;
+      delete key.publicKey.alg;
+      delete key.privateKey.alg;
+      delete key.publicKey.key_ops;
+      delete key.privateKey.key_ops;
+      const res =  (JSON.stringify(objectSort(jwkpub)) === JSON.stringify(objectSort(key.publicKey)))
+          && (JSON.stringify(objectSort(jwkpri)) === JSON.stringify(objectSort(key.privateKey)));
       if (!res) {
         console.log(objectSort(jwkpub));
-        console.log(objectSort(key.publicKey.key));
+        console.log(objectSort(key.publicKey));
         console.log(objectSort(jwkpri));
-        console.log(objectSort(key.privateKey.key));
+        console.log(objectSort(key.privateKey));
         // console.log(pempub);
         // console.log(pempri);
       }
@@ -58,20 +56,20 @@ describe('EC Key conversion from/to JWK test.', () => {
   it('JWK EC should be successfully converted to PEM and re-converted to JWK correctly with public key compact form', async () => {
 
     const array = ECKeySet.map( (key) => {
-      const pempub = keyutils.fromJwkTo('pem', key.publicKey.key, 'public', {compact: true});
-      const pempri = keyutils.fromJwkTo('pem', key.privateKey.key, 'private', {compact: true});
+      const pempub = keyutils.fromJwkTo('pem', key.publicKey, 'public', {compact: true});
+      const pempri = keyutils.fromJwkTo('pem', key.privateKey, 'private', {compact: true});
       // console.log(pempub);
       // console.log(pempri);
       const jwkpub = keyutils.toJwkFrom('pem', pempub, 'public');
       const jwkpri = keyutils.toJwkFrom('pem', pempri, 'private');
-      delete key.publicKey.key.ext;
-      delete key.privateKey.key.ext;
-      delete key.publicKey.key.alg;
-      delete key.privateKey.key.alg;
-      delete key.publicKey.key.key_ops;
-      delete key.privateKey.key.key_ops;
-      return (JSON.stringify(objectSort(jwkpub)) === JSON.stringify(objectSort(key.publicKey.key)))
-          && (JSON.stringify(objectSort(jwkpri)) === JSON.stringify(objectSort(key.privateKey.key)));
+      delete key.publicKey.ext;
+      delete key.privateKey.ext;
+      delete key.publicKey.alg;
+      delete key.privateKey.alg;
+      delete key.publicKey.key_ops;
+      delete key.privateKey.key_ops;
+      return (JSON.stringify(objectSort(jwkpub)) === JSON.stringify(objectSort(key.publicKey)))
+          && (JSON.stringify(objectSort(jwkpri)) === JSON.stringify(objectSort(key.privateKey)));
     });
     console.log(array);
     expect(array.every( (elem) => elem)).to.be.true;
@@ -81,19 +79,19 @@ describe('EC Key conversion from/to JWK test.', () => {
   it('JWK EC should be successfully converted to DER and re-converted to JWK correctly', async () => {
 
     const array = ECKeySet.map( (key) => {
-      const derpub = keyutils.fromJwkTo('der', key.publicKey.key, 'public', {compact: false});
-      const derpri = keyutils.fromJwkTo('der', key.privateKey.key, 'private', {compact: false});
+      const derpub = keyutils.fromJwkTo('der', key.publicKey, 'public', {compact: false});
+      const derpri = keyutils.fromJwkTo('der', key.privateKey, 'private', {compact: false});
 
       const jwkpub = keyutils.toJwkFrom('der', derpub, 'public');
       const jwkpri = keyutils.toJwkFrom('der', derpri, 'private');
-      delete key.publicKey.key.ext;
-      delete key.privateKey.key.ext;
-      delete key.publicKey.key.alg;
-      delete key.privateKey.key.alg;
-      delete key.publicKey.key.key_ops;
-      delete key.privateKey.key.key_ops;
-      return (JSON.stringify(objectSort(jwkpub)) === JSON.stringify(objectSort(key.publicKey.key)))
-          && (JSON.stringify(objectSort(jwkpri)) === JSON.stringify(objectSort(key.privateKey.key)));
+      delete key.publicKey.ext;
+      delete key.privateKey.ext;
+      delete key.publicKey.alg;
+      delete key.privateKey.alg;
+      delete key.publicKey.key_ops;
+      delete key.privateKey.key_ops;
+      return (JSON.stringify(objectSort(jwkpub)) === JSON.stringify(objectSort(key.publicKey)))
+          && (JSON.stringify(objectSort(jwkpri)) === JSON.stringify(objectSort(key.privateKey)));
     });
     console.log(array);
     expect(array.every( (elem) => elem)).to.be.true;
@@ -101,19 +99,19 @@ describe('EC Key conversion from/to JWK test.', () => {
 
   it('JWK EC should be successfully converted to DER and re-converted to JWK correctly with public key compact form', async () => {
     const array = ECKeySet.map( (key) => {
-      const derpub = keyutils.fromJwkTo('der', key.publicKey.key, 'public', {compact: true});
-      const derpri = keyutils.fromJwkTo('der', key.privateKey.key, 'private', {compact: true});
+      const derpub = keyutils.fromJwkTo('der', key.publicKey, 'public', {compact: true});
+      const derpri = keyutils.fromJwkTo('der', key.privateKey, 'private', {compact: true});
 
       const jwkpub = keyutils.toJwkFrom('der', derpub, 'public');
       const jwkpri = keyutils.toJwkFrom('der', derpri, 'private');
-      delete key.publicKey.key.ext;
-      delete key.privateKey.key.ext;
-      delete key.publicKey.key.alg;
-      delete key.privateKey.key.alg;
-      delete key.publicKey.key.key_ops;
-      delete key.privateKey.key.key_ops;
-      return (JSON.stringify(objectSort(jwkpub)) === JSON.stringify(objectSort(key.publicKey.key)))
-          && (JSON.stringify(objectSort(jwkpri)) === JSON.stringify(objectSort(key.privateKey.key)));
+      delete key.publicKey.ext;
+      delete key.privateKey.ext;
+      delete key.publicKey.alg;
+      delete key.privateKey.alg;
+      delete key.publicKey.key_ops;
+      delete key.privateKey.key_ops;
+      return (JSON.stringify(objectSort(jwkpub)) === JSON.stringify(objectSort(key.publicKey)))
+          && (JSON.stringify(objectSort(jwkpri)) === JSON.stringify(objectSort(key.privateKey)));
     });
     console.log(array);
     expect(array.every( (elem) => elem)).to.be.true;
@@ -121,21 +119,21 @@ describe('EC Key conversion from/to JWK test.', () => {
 
   it('JWK EC should be successfully converted to uncompressed-octet formed key and vice varsa', async () => {
     const array = ECKeySet.map( (key) => {
-      const namedCurve = key.publicKey.key.crv;
-      const octpub = keyutils.fromJwkTo('oct', key.publicKey.key, 'public', {format: 'string', compact: false});
-      const octpri = keyutils.fromJwkTo('oct', key.privateKey.key, 'private', {format: 'string', compact: false});
+      const namedCurve = key.publicKey.crv;
+      const octpub = keyutils.fromJwkTo('oct', key.publicKey, 'public', {format: 'string', compact: false});
+      const octpri = keyutils.fromJwkTo('oct', key.privateKey, 'private', {format: 'string', compact: false});
       // console.log(octpub);
       // console.log(octpri);
       const jwkpub = keyutils.toJwkFrom('oct', octpub, 'public', {format: 'string', namedCurve });
       const jwkpri = keyutils.toJwkFrom('oct', octpri, 'private', {format: 'string', namedCurve });
-      delete key.publicKey.key.ext;
-      delete key.privateKey.key.ext;
-      delete key.publicKey.key.alg;
-      delete key.privateKey.key.alg;
-      delete key.publicKey.key.key_ops;
-      delete key.privateKey.key.key_ops;
-      return (JSON.stringify(objectSort(jwkpub)) === JSON.stringify(objectSort(key.publicKey.key)))
-          && (JSON.stringify(objectSort(jwkpri)) === JSON.stringify(objectSort(key.privateKey.key)));
+      delete key.publicKey.ext;
+      delete key.privateKey.ext;
+      delete key.publicKey.alg;
+      delete key.privateKey.alg;
+      delete key.publicKey.key_ops;
+      delete key.privateKey.key_ops;
+      return (JSON.stringify(objectSort(jwkpub)) === JSON.stringify(objectSort(key.publicKey)))
+          && (JSON.stringify(objectSort(jwkpri)) === JSON.stringify(objectSort(key.privateKey)));
     });
     console.log(array);
     expect(array.every( (elem) => elem)).to.be.true;
@@ -143,21 +141,21 @@ describe('EC Key conversion from/to JWK test.', () => {
 
   it('JWK EC should be successfully converted to compact-octet formed key and vice varsa', async () => {
     const array = ECKeySet.map( (key) => {
-      const namedCurve = key.publicKey.key.crv;
-      const octpub = keyutils.fromJwkTo('oct', key.publicKey.key, 'public', {format: 'string', compact: true});
-      const octpri = keyutils.fromJwkTo('oct', key.privateKey.key, 'private', {format: 'string', compact: true});
+      const namedCurve = key.publicKey.crv;
+      const octpub = keyutils.fromJwkTo('oct', key.publicKey, 'public', {format: 'string', compact: true});
+      const octpri = keyutils.fromJwkTo('oct', key.privateKey, 'private', {format: 'string', compact: true});
       // console.log(octpub);
       // console.log(octpri);
       const jwkpub = keyutils.toJwkFrom('oct', octpub, 'public', {format: 'string', namedCurve });
       const jwkpri = keyutils.toJwkFrom('oct', octpri, 'private', {format: 'string', namedCurve });
-      delete key.publicKey.key.ext;
-      delete key.privateKey.key.ext;
-      delete key.publicKey.key.alg;
-      delete key.privateKey.key.alg;
-      delete key.publicKey.key.key_ops;
-      delete key.privateKey.key.key_ops;
-      return (JSON.stringify(objectSort(jwkpub)) === JSON.stringify(objectSort(key.publicKey.key)))
-          && (JSON.stringify(objectSort(jwkpri)) === JSON.stringify(objectSort(key.privateKey.key)));
+      delete key.publicKey.ext;
+      delete key.privateKey.ext;
+      delete key.publicKey.alg;
+      delete key.privateKey.alg;
+      delete key.publicKey.key_ops;
+      delete key.privateKey.key_ops;
+      return (JSON.stringify(objectSort(jwkpub)) === JSON.stringify(objectSort(key.publicKey)))
+          && (JSON.stringify(objectSort(jwkpri)) === JSON.stringify(objectSort(key.privateKey)));
     });
     console.log(array);
     expect(array.every( (elem) => elem)).to.be.true;
--- packages/js_crypto_key_utils/test/jwkthumbprint.bundle.spec.js
+++ packages/js_crypto_key_utils/test/jwkthumbprint.bundle.spec.js
@@ -1,5 +1,5 @@
 import keyutils from '../dist/jsckeyutil.bundle.js';
-import jscu from 'js-crypto-utils';
+import ec from 'js-crypto-ec/dist/index.js';
 import sampleRSA from './rsa_sample.js';
 
 import chai from 'chai';
@@ -10,13 +10,11 @@ const curves = ['P-256', 'P-384', 'P-521'];
 const bits = ['2048', '4096'];
 const hashes = [ 'SHA-256', 'SHA-384', 'SHA-512'];
 describe('JWK thumbprint generation test.', () => {
-
-  const getKeyParam = (elem) => ({keyType: 'EC', namedCurve: elem});
+  
   let keySet = [];
   before(async () => {
     keySet = await Promise.all(curves.map(async (crv) => {
-      const param = getKeyParam(crv);
-      return await jscu.crypto.generateKeyPair(param);
+      return await ec.generateKey(crv);
     }));
   });
 
@@ -24,7 +22,7 @@ describe('JWK thumbprint generation test.', () => {
     const tps = await Promise.all( keySet.map(
       async (jwkey) => await Promise.all( hashes.map(
         async (h) => {
-          const tp = await keyutils.getJwkThumbprint(jwkey.publicKey.key, h);
+          const tp = await keyutils.getJwkThumbprint(jwkey.publicKey, h);
           expect(tp instanceof Uint8Array).to.be.true;
           return tp;
         }
@@ -36,7 +34,7 @@ describe('JWK thumbprint generation test.', () => {
     const tps = await Promise.all( keySet.map(
       async (jwkey) => await Promise.all( hashes.map(
         async (h) => {
-          const tp = await keyutils.getJwkThumbprint(jwkey.publicKey.key, h, 'hex');
+          const tp = await keyutils.getJwkThumbprint(jwkey.publicKey, h, 'hex');
           expect(typeof(tp)==='string').to.be.true;
           return tp;
         }
