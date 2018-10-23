0b0b8e1ea9a5665c75cfd5775191f2ffe341cd14
Jun Kurihara <kurihara@ieee.org>
2018-10-23 22:00:13 +0900

    Updated: supported new library project skeleton

--- packages/js_crypto_random/test/random.spec.js
+++ packages/js_crypto_random/test/random.spec.js
@@ -1,29 +1,13 @@
-let env;
-let random;
-if(process.env.TEST_ENV === 'bundle'){
-  env = 'From bundle';
-  random = require('../dist/jscrandom.bundle.js');
-}
-else if (process.env.TEST_ENV === 'window'){
-  env = 'From window';
-  if(typeof window !== 'undefined' && typeof window.jscrandom !== 'undefined'){
-    random = window.jscrandom;
-  }
-  else{
-    random = require('../src/index.js');
-  }
-}
-else {
-  env = 'From source';
-  random = require('../src/index.js');
-}
-
 import chai from 'chai';
 // const should = chai.should();
 const expect = chai.expect;
+import {getTestEnv} from './prepare.js';
+const env = getTestEnv();
+const random = env.library;
+const envName = env.envName;
 
 
-describe(`${env}: Random generation test`, () => {
+describe(`${envName}: Random generation test`, () => {
 
   it('Random bytes of desired length should be generated successfully', () => {
     const r = random.getRandomBytes(32);
