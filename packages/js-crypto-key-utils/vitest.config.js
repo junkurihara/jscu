7bf3c541f5767227619786e6778ad680378e6a33
Jun Kurihara <junkurihara@users.noreply.github.com>
2025-05-29 21:12:57 +0900

    fix timeout

--- packages/js-crypto-key-utils/vitest.config.js
+++ packages/js-crypto-key-utils/vitest.config.js
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: 'node',
-    include: ['**/test/**/*.spec.ts'],
+    environment: "node",
+    include: ["**/test/**/*.spec.ts"],
     globals: true,
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html', 'lcov'],
-      include: ['src/**/*.ts'],
-      exclude: ['test/**/*', '**/*.d.ts', '**/*.spec.ts'],
-      reportsDirectory: 'coverage/vitest'
-    }
-  }
+      provider: "v8",
+      reporter: ["text", "html", "lcov"],
+      include: ["src/**/*.ts"],
+      exclude: ["test/**/*", "**/*.d.ts", "**/*.spec.ts"],
+      reportsDirectory: "coverage/vitest",
+    },
+    // Global test timeout
+    testTimeout: 30000,
+  },
 });
